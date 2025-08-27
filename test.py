d1d44d424ebd2cb9a4c66919118ec267bc2bc5f6
Akshit mewara <108535777+Akshitmewara@users.noreply.github.com>
2025-08-27 22:06:10 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import pyodbc
-
-conn = pyodbc.connect(
-    'DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=msdb;UID=sa;PWD=NewStrongPassword123!'
-)
-cursor = conn.cursor()
-cursor.execute("SELECT 1")
-print(cursor.fetchone())
-conn.close()
